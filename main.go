e9144b0b7460d73a48ba60cd83fdbadd91a73534
Christopher Nguyen <christoph.nguyen@gmail.com>
2015-01-05 14:53:08 -0800

    Test urls working

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+	"strings"
+	"sync"
+)
+
+var file = flag.String("file", "", "path to file to read (newline delimited)")
+
+func main() {
+	flag.Parse()
+
+	f, err := ioutil.ReadFile(*file)
+	if err != nil {
+		panic(err)
+	}
+
+	var wg sync.WaitGroup
+	urls := strings.Split(string(f), "\n")
+	var validProtocol = regexp.MustCompile(`^http`)
+
+	for _, url := range urls {
+		if len(url) > 0 {
+			wg.Add(1)
+			go func(url string) {
+				defer wg.Done()
+				if !validProtocol.MatchString(url) {
+					fmt.Printf("URL %s does not have protocol, using http\n", url)
+					url = "http://" + url
+				}
+				resp, err := http.Get(url)
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Printf("%+v\n", resp)
+					if resp.StatusCode >= 300 {
+						fmt.Printf("%s returned with %d\n", url, resp.StatusCode)
+					}
+				}
+			}(url)
+		}
+	}
+	wg.Wait()
+}
